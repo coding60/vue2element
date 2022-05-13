3225ec6755a362a85d8df2c5ec034e36e94e394f
coding60 <149486939@qq.com>
2022-05-13 22:17:31 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -24,6 +24,26 @@ import router from './router'
 // })
 Vue.use(ElementUI);
 new Vue({
-  render: h => h(App),
-  router
+	render: h => h(App),
+	router
 }).$mount('#app')
+router.beforeEach((to, from, next) => {
+	if (to.meta.requireAuth) { // 判断该路由是否需要登录权限
+		let logins = 1
+		//console.log('beforeEach获取当前的token是否存在  '+store.state.loginModule.token)
+		//if (store.state.loginModule.token) {  // 通过vuex state获取当前的token是否存在
+		if (confirm('确实要登录吗?')) {
+			next();
+		} else {
+			next({
+				path: '/login',
+				query: {
+					redirect: to.fullPath
+				} // 将跳转的路由path作为参数，登录成功后跳转到该路由
+			})
+		}
+		// alert('需要权限')
+	} else {
+		next();
+	}
+});
